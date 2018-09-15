d6790f932d7b63db1679b46153ebe918b43e1166
Fernando Carranza <foreverfas_fc@hotmail.com>
2018-09-15 16:10:26 -0600

    Cambios API

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-	String url = "jdbc:mysql://localhost:3306/";
+	String url = "jdbc:mysql://rentadb.c9bf3mte5srb.us-east-2.rds.amazonaws.com:3306/";
 	String dbName = "rentadb";
 	String driver = "com.mysql.jdbc.Driver";
-	String userName = "root";
-	String password = "";
+	String userName = "adminrenta";
+	String password = "fslrenta";
 	
 	public Connection conn()
 			throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
